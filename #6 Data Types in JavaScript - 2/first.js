1cea59ae7915b57399f7ec4dcd418a60e6ff088b
themdshakibul <mdshakibul88240@gmail.com>
2026-01-22 19:36:39 +0600

    data type javaScript

---
+++ #6 Data Types in JavaScript - 2/first.js
@@ -0,0 +1,51 @@
+let usur = "themdshakibul"; // String
+
+console.log(usur); // themdshakibul
+
+let fristName = "Shakibul"; // String
+let lastName = "Islam"; // String
+
+let user = fristName + lastName; // ShakibulIslam
+
+let user1 = 'Navin Reddy "Teluska"';
+
+let user2 = "Navin \nReddy";
+
+console.log(user2);
+
+// Navin
+// Reddy
+
+console.log(user1); // Navin Reddy "Teluska"
+
+console.log(user); // ShakibulIslam
+
+// boolean data type
+
+// let bool = 5 > 6; // false
+
+let bool = 5 < 6; // true
+
+console.log(typeof bool);
+
+// null data type
+
+let data = null; // null
+
+console.log(data); // null
+
+console.log(typeof data); // object
+
+// undefined data type
+
+let data1; // undefined
+
+console.log(data1); // undefined
+
+console.log(typeof data1); // undefined
+
+let num = 5;
+
+console.log(5 / "Shakibul"); // NaN
+
+console.log(typeof (5 / "Shakibul")); // number
