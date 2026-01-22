da9d70451847ab801c055c36e618efa383c81395
themdshakibul <mdshakibul88240@gmail.com>
2026-01-22 07:13:07 +0600

    data type

---
+++ data type/first.js
@@ -0,0 +1,17 @@
+let data = 6; // Number
+let name = "Shakibul"; // String
+
+let num1 = 7;
+console.log(typeof num1); // number
+
+let student = true;
+console.log(typeof student); // boolean
+
+let x;
+console.log(typeof x); // undefined
+
+let num2 = 0xff;
+console.log(num2); // 255
+
+let num3 = -5 / 0;
+console.log(Number.MAX_VALUE * 10); // Ibnfinity
