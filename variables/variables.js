f39ba39044c92b99b86b27b785977131ea10b282
themdshakibul <mdshakibul88240@gmail.com>
2026-01-22 06:06:08 +0600

    variables

---
+++ variables/variables.js
@@ -0,0 +1,35 @@
+// num = 2 + 2; // 4
+
+// console.log(num); // 4
+
+// console.log(num + 3); // 7
+
+let num = 4; // Declare
+
+console.log(num); // 4
+
+num = 5; // Re-assign
+
+console.log(num); //5
+
+// variable name
+// 1. can have characters
+// 2. can  have number except first position
+// 3. can have spacial symbols $ and_
+
+let urser_name = "themdshakibul"; // _ snake casing rule
+
+let userName = "themdshakibul"; // camel casing
+
+let UserName = "themdshakibul"; // Pascal casing
+
+console.log(urser_name);
+
+// let username = Shakibul  // error
+let username = "Shakibul"; // String হিসেবে ডিক্লেয়ার করা হয়
+
+console.log(username); // Shakibul
+
+let account = "this is' my account";
+
+console.log(account);
